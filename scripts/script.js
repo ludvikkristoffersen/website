2bad5009a69342d26119804a7965a5c919732963
Ludde <70239345+ludvikkristoffersen@users.noreply.github.com>
2024-01-17 02:26:12 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ document.getElementById("linkedinbutton").addEventListener("click", function() {
     window.location.href = "https://www.linkedin.com/in/ludvik-kristoffersen/";
 });
 document.getElementById("githubbutton").addEventListener("click", function() {
-    window.location.href = "https://github.com/luddekn";
+    window.location.href = "https://github.com/ludvikkristoffersen";
 });
 document.getElementById("mediumbutton").addEventListener("click", function() {
     window.location.href = "https://ludvikkristoffersen.medium.com/";
