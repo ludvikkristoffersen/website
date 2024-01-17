7b659bae6dceb9640119b920f484881a8e7e8952
Ludde <70239345+luddekn@users.noreply.github.com>
2024-01-17 02:04:17 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+document.getElementById("linkedinbutton").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/ludvik-kristoffersen/";
+});
+document.getElementById("githubbutton").addEventListener("click", function() {
+    window.location.href = "https://github.com/luddekn";
+});
+document.getElementById("mediumbutton").addEventListener("click", function() {
+    window.location.href = "https://ludvikkristoffersen.medium.com/";
+});
+document.getElementById("certificatebutton").addEventListener("click", function() {
+    window.location.href = "certificates.html";
+});
